ac20f442b0a3ed3a506b45da5c3812fc6a911a54
andron94 <makedonsky94@gmail.com>
2025-10-21 19:46:18 -0700

    Use "TAB" to cycle through "outline" entries.

--- init.el
+++ init.el
@@ -1,5 +1,10 @@
 ;; -*- lexical-binding: t; -*-
 
+;;; Organizer
+(use-package outline
+  :ensure nil
+  :config
+  (setq outline-minor-mode-cycle t))
 ;;; Footer
 ;; Local Variables:
 ;; eval: (outline-minor-mode 1)
