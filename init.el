eecb0106ccad2c8a07fce9c4801a7575a1fdbac5
andron94 <makedonsky94@gmail.com>
2025-12-08 13:42:53 -0800

    Set evil key bindings for "occur" mode.

--- init.el
+++ init.el
@@ -335,7 +335,7 @@ and Emacs states.")
   :after evil
   :config
   (setq evil-collection-key-blacklist `(,my-leader-key ,my-leader-alt-key))
-  (evil-collection-init '(dired elpaca ibuffer magit outline xref)))
+  (evil-collection-init '(dired elpaca ibuffer magit outline replace xref)))
 ;;; UI/UX
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
