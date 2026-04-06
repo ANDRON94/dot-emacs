38b34064dcfa27842a2b5242cb526918d43148cd
andron94 <makedonsky94@gmail.com>
2026-04-05 18:05:51 -0700

    Update "elpaca" installer.

--- init.el
+++ init.el
@@ -290,26 +290,27 @@ and Emacs states.")
   :doc "Keymap for user-defined key bindings inside lisp interaction mode."
   :keymap my-emacs-lisp-mode-map)
 ;;; Elpaca
-(defvar elpaca-installer-version 0.11)
+(defvar elpaca-installer-version 0.12)
 
 (defvar elpaca-directory (my-etc "elpaca/"))
 
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 
-(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-sources-directory (expand-file-name "sources/" elpaca-directory))
 
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
                               :ref nil :depth 1 :inherit ignore
                               :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-                              :build (:not elpaca--activate-package)))
+                              :build (:not elpaca-activate)))
 
-(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-sources-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
        (order (cdr elpaca-order))
        (default-directory repo))
   (add-to-list 'load-path (if (file-exists-p build) build repo))
   (unless (file-exists-p repo)
     (make-directory repo t)
+    (when (<= emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
         (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
                   ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
